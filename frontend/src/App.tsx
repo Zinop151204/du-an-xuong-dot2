5634ff7ce3ecfdfd12d40abadeece9e3ab55c6ae
Huytrong1512 <trongnhph32253@fpt.edu.vn>
2024-04-07 15:48:05 +0700

    update2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,11 @@
 import { Toaster } from '@/components/ui/toaster'
-import { Route, Routes } from 'react-router-dom'
-import Categories from './components/view/Categories'
-import CategoryDetail from './pages/user/DetailCategory'
-import DetailProduct from './pages/user/DetailProduct'
-import Home from './pages/user/Home'
-import ShopSP from './pages/user/Shop'
-import OutletAdmin from './viewWedsite/OutletAdmin'
-import OutletView from './viewWedsite/OutletView'
-import Signin from './pages/login/signin'
-import Cart from './pages/user/Cart'
-import ProductManagement from './pages/(dashboard)/_components'
-import ProductEditPage from './pages/(dashboard)/_components/edit'
-import ProductAddPage from './pages/(dashboard)/_components/add'
-import List from './pages/(dashboard)/_components/list'
-import Order from './pages/user/Order'
+import Router from './router'
 
 function App() {
 
   return (
     <>
-      <Routes>
-
-        <Route path='/' element={<OutletView />} >
-          <Route index element={<Home />} />
-          <Route path='shop' element={<ShopSP />} />
-          <Route path='/categories' element={<Categories/>}/>
-          <Route path='/detailcategory/:id' element={<CategoryDetail/>}/>
-          <Route path='products/:id' element={<DetailProduct />} />
-          <Route path='signin' element={<Signin/>}/>
-          <Route path='carts' element={<Cart/>}/>
-          <Route path='order' element={<Order/>}/>
-        </Route>
-
-        <Route path='admin' element={<OutletAdmin />} >
-          <Route index element={<List/>}/>
-          <Route path='products/:id/edit' element={<ProductEditPage />} />
-          <Route path='products/add' element={<ProductAddPage />} />
-        </Route>
-
-      </Routes>
+      <Router/>
       <Toaster/>
 
     </>
---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,49 @@
+import { Toaster } from '@/components/ui/toaster'
+import { Route, Routes } from 'react-router-dom'
+
+import Categories from '@/components/view/Categories'
+import LayoutAdmin from '@/pages/(dashboard)/layout'
+import ProductAddPage from '@/pages/(dashboard)/product/_components/add'
+import ProductEditPage from '@/pages/(dashboard)/product/_components/edit'
+import ProductManagement from '@/pages/(dashboard)/product/page'
+import Signin from '@/pages/login/signin'
+import Cart from '@/pages/user/Cart'
+import CategoryDetail from '@/pages/user/DetailCategory'
+import DetailProduct from '@/pages/user/DetailProduct'
+import Order from '@/pages/user/Order'
+import ShopSP from '@/pages/user/Shop'
+import OutletView from '@/viewWedsite/OutletView'
+import Home from '@/pages/user/Home'
+
+
+function Router() {
+
+  return (
+    <>
+      <Routes>
+
+      <Route path='/' element={<OutletView />} >
+          <Route index element={<Home />} />
+          <Route path='shop' element={<ShopSP />} />
+          <Route path='/categories' element={<Categories/>}/>
+          <Route path='/detailcategory/:id' element={<CategoryDetail/>}/>
+          <Route path='products/:id' element={<DetailProduct />} />
+          <Route path='signin' element={<Signin/>}/>
+          <Route path='carts' element={<Cart/>}/>
+          <Route path='order' element={<Order/>}/>
+        </Route>
+
+        <Route path='admin' element={<LayoutAdmin />} >
+          <Route path='products' element={<ProductManagement/>}/>
+          <Route path='products/:id/edit' element={<ProductEditPage />} />
+          <Route path='products/add' element={<ProductAddPage />} />
+        </Route>
+
+      </Routes>
+      <Toaster/>
+
+    </>
+  )
+}
+
+export default Router
